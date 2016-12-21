eec7e7a4f3294afbac7d6d1c373afd685be8a3a3
deathcod <chinmay.rakshit@gmail.com>
2016-12-21 20:58:32 +0530

    update

--- Django_projects/basic_project/user_info/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import info
-
-admin.site.register(info)
-# Register your models here.
--- Django_projects/basic_project/user_info/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class User_infoConfig(AppConfig):
-	name = 'user_info'
