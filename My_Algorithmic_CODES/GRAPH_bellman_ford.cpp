da65a2508834705d6498a628da67d252784e1fd9
deathcod <chinmay.rakshit@gmail.com>
2017-06-19 12:50:47 +0530

    updated bellman ford algorithm

--- My_Algorithmic_CODES/GRAPH_bellman_ford.cpp
+++ My_Algorithmic_CODES/GRAPH_bellman_ford.cpp
@@ -1,7 +1,18 @@
 /*
-codemonk : monk's business day
 bellman ford  
-by chinmay rakshit 14/11/2015
+-------------
+the algorithm calculate shortest paths in bottom-up manner. It first calculates the shortest distances for the shortest paths which have at-most one edge in the path. Then, it calculates shortest paths with at-nost 2 edges, and so on. After the ith iteration of outer loop, the shortest paths with at most i edges are calculated. There can be maximum |V| – 1 edges in any simple path, that is why the outer loop runs |v| – 1 times. The idea is, assuming that there is no negative weight cycle, if we have calculated shortest paths with at most i edges, then an iteration over all edges guarantees to give shortest path with at-most (i+1) edges
+
+Time complexity : O(VE)
+Space complexity : O(V)
+
+It depends on the way we define it.
+
+*   If we assume that the graph is given, the extra space complexity is O(V) (for an array of distances).
+
+*   If we assume that the graph also counts, it can be O(V^2) for an adjacency matrix and O(V+E) for an adjacency list.
+
+They both are "true" in some sense. It's just about what we want to count in a specific problem
 */
 
 #include <bits/stdc++.h>
@@ -25,23 +36,21 @@ int main()
         {
             for(int j=0;j<m;j++)
             {
-                if(visited[v[j][1]]<visited[v[j][0]]+v[j][2])
+                if(visited[v[j][1]]>visited[v[j][0]]+v[j][2])
                     visited[v[j][1]]=visited[v[j][0]]+v[j][2];
             }
         }
         int flag=0;
         for(int i=0;i<m;i++)
         {
-            if(visited[v[i][1]]<visited[v[i][0]]+v[i][2])
+            if(visited[v[i][1]]>visited[v[i][0]]+v[i][2])
             {
                 flag=1;
                 break;
             }
         }
-        if(flag==1)
-            printf("Yes\n");
-        else
-            printf("No\n");
+        //get the value 
+        printf("%d\n",visited[n-1] );
     }
     return 0;
 }
